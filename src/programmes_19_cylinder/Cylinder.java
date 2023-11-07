68a735940da874ad833f2a18a6ff43c73220f932
Goswamiamruta <amrutagoswami87@gmail.com>
2023-11-07 22:40:01 +0000

    my java homework week 04

---
+++ src/programmes_19_cylinder/Cylinder.java
@@ -0,0 +1,25 @@
+package programmes_19_cylinder;
+
+public class Cylinder extends Circle1 {
+    double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return getArea() * getHeight();
+    }
+}
+
+
+
